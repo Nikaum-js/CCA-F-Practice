aed2a367de995badca247776df9e5bf0ba3153cc
Nikolas Santana <nikolasdssantana@gmail.com>
2026-02-18 16:07:55 -0300

    🎨 feat: bootstrap app shell, entry point and utils

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from '@/App'
+import { LangProvider } from '@/hooks/useLang'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <LangProvider>
+      <App />
+    </LangProvider>
+  </StrictMode>,
+)
